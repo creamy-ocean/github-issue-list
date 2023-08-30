dd0d5300a4bb335c6102e2aeb6a361c76eca4fb7
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-08-30 20:50:26 +0900

    [Feat] 에러 페이지 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import IssueList from "../components/domain/IssueList";
 import IssueDetail from "../components/domain/IssueDetail";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
