aaf902660414aa1bb830e999fd96ecef1fba5579
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-08-30 17:21:27 +0900

    [Feat] 공통 헤더 추가
    - Organization Name/ Repository Name 표시되도록 공통 헤더 추가

--- src/components/common/Root.tsx
+++ src/components/common/Root.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Heading from "./Heading";
+import { ORGANIZATION_NAME, REPOSITORY_NAME } from "../../info";
 
 const Root = () => {
   return (
     <>
-      <Heading>Github Issue List</Heading>
+      <Heading>
+        {ORGANIZATION_NAME} / {REPOSITORY_NAME}
+      </Heading>
       <main>
         <Outlet />
       </main>
